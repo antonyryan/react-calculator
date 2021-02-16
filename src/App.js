322c294bc2ad75ed71c9f1f295295c0bd406e474
Ryan <antonyryanr@gmail.com>
2021-02-15 21:15:14 -0500

    Initial setup. Calculator tracking previous expression and evaluating when operation selected

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import './App.scss';
 
-function App() {
+const App = () => {
+  const [previousValue, setPreviousValue] = useState(0);
+  const [total, setTotal] = useState(0);
+  let [userInput, setUserInput] = useState('');
+  
+  const appendPrevious = (e) => {
+    setUserInput(userInput += e.target.value);
+  };
+
+  const handleOperation = (e) => {
+
+    if (previousValue) {
+      let expressionStr = eval(`${previousValue} ${userInput}`);
+      let previous = `${expressionStr} ${e.target.value}`;
+
+      setPreviousValue(previous);
+      setUserInput('');
+    } else {
+      let previous = `${userInput.slice()} ${e.target.value}`;
+
+      setPreviousValue(previous);
+      setUserInput('');
+    }
+  }
+
+  const clearTotal = () => {
+    setTotal(0);
+    setUserInput('');
+    setPreviousValue(0);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator">
+      <div className="calculator__container">
+        <div className="calculator__output">
+          <span className="calculator__input">{previousValue ? previousValue : ''}</span>
+          <span className="calculator__total">{userInput ? userInput : total}</span>
+        </div>
+        <div className="calculator__options">
+          <button className="calculator__option calculator__option--operation" onClick={clearTotal}>C</button>
+          <button className="calculator__option calculator__option--operation">±</button>
+          <button className="calculator__option calculator__option--operation">%</button>
+          <button className="calculator__option calculator__option--operation" value="/" onClick={(e) => handleOperation(e)}>÷</button>
+          <button className="calculator__option" value="7" onClick={(e) => appendPrevious(e)}>7</button>
+          <button className="calculator__option" value="8" onClick={(e) => appendPrevious(e)}>8</button>
+          <button className="calculator__option" value="9" onClick={(e) => appendPrevious(e)}>9</button>
+          <button className="calculator__option calculator__option--operation" value="*" onClick={(e) => handleOperation(e)}>×</button>
+          <button className="calculator__option" value="4" onClick={(e) => appendPrevious(e)}>4</button>
+          <button className="calculator__option" value="5" onClick={(e) => appendPrevious(e)}>5</button>
+          <button className="calculator__option" value="6" onClick={(e) => appendPrevious(e)}>6</button>
+          <button className="calculator__option calculator__option--operation" value="-" onClick={(e) => handleOperation(e)}>-</button>
+          <button className="calculator__option" value="1" onClick={(e) => appendPrevious(e)}>1</button>
+          <button className="calculator__option" value="2" onClick={(e) => appendPrevious(e)}>2</button>
+          <button className="calculator__option" value="3" onClick={(e) => appendPrevious(e)}>3</button>
+          <button className="calculator__option calculator__option--operation" value="+" onClick={(e) => handleOperation(e)}>+</button>
+          <button className="calculator__option" value="0" onClick={(e) => appendPrevious(e)}>0</button>
+          <button className="calculator__option" value="." onClick={(e) => appendPrevious(e)} disabled={userInput.indexOf('.') !== -1}>.</button>
+          <div></div>
+          <button className="calculator__option calculator__option--operation">=</button>
+        </div>
+      </div>
     </div>
   );
 }
